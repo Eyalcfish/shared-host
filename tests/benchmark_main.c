0cfc8d23e680a0f9dbe5809fcb77fbffed7b6558
fishministrator <yulucohen@gmail.com>
2026-04-04 01:54:56 +0300

    formatrtingf

--- tests/benchmark_main.c
+++ tests/benchmark_main.c
@@ -4,21 +4,22 @@
 
 #define ITERS 1000
 
-int main() {
-  printf("Starting benchmark...\n");
-  clock_t start = clock();
+int main()
+{
+    printf("Starting benchmark...\n");
+    clock_t start = clock();
 
-  for (int i = 0; i < ITERS; i++) {
-    shared_host_connection *conn = NULL;
-    char port[64];
-    sprintf(port, "bench_port_%d", i);
-    create_shared_host_connection(port, 1024, &conn);
-    close_shared_host_connection(conn);
-  }
+    for (int i = 0; i < ITERS; i++)
+    {
+        shared_host_connection *conn = NULL;
+        char port[64];
+        sprintf(port, "bench_port_%d", i);
+        create_shared_host_connection(port, 1024, &conn);
+        close_shared_host_connection(conn);
+    }
 
-  clock_t end = clock();
-  double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-  printf("Benchmark: %f seconds for %d connection creations and closures.\n",
-         time_spent, ITERS);
-  return 0;
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Benchmark: %f seconds for %d connection creations and closures.\n", time_spent, ITERS);
+    return 0;
 }
